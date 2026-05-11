aba10f8d024a7610455ab899d1fb243d1ab4ff0e
oleksandr <oleksandr.k.kodehode@gmail.com>
2026-05-11 10:27:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const paper = new Node("paper");
+const rock = new Node("rock");
+const scissors = new Node("scissors");
+
+paper.next = rock;
+rock.next = scissors;
+scissors.next = paper;
+
+console.log(paper.next);
+console.log(rock.next);
+console.log(scissors.next);
+console.log(paper.next.next);
+
+function rockPaperScissors(myGuess) {
+    console.log(
+        "The game is simple.\n You choose ['rock', 'paper', 'scissors'] same does the robot and the combinations are compared.\n If you struggle with understand the rules... Seems like you have some serious problems...(((\n",
+    );
+    let unknownResult =
+        "Something went wrong!\n (As mentioned in the rules... I guess you have a tough life!)\n";
+    const allowedAnswers = ["paper", "rock", "scissors"];
+    if (!allowedAnswers.includes(myGuess)) {
+        return unknownResult;
+    }
+
+    const steps = Math.floor(Math.random() * 3);
+
+    let currentNode = paper;
+    for (let i = 0; i < steps; i++) {
+        currentNode = currentNode.next;
+    }
+
+    let robotGuess = currentNode.value;
+    console.log(`Robot choose -> ${robotGuess}!\n`);
+    myGuess = myGuess.toLowerCase();
+    console.log(`You choose -> ${myGuess}!\n`);
+
+    if (currentNode.value === myGuess) {
+        return "Ai is getting closer! (Draw)\n";
+    } else if (currentNode.next.value === myGuess) {
+        return "You lose!\n";
+    } else if (currentNode.next.next.value === myGuess) {
+        return "Congrats! You win!\n";
+    }
+}
+
+console.log(rockPaperScissors("paper"));
+
+// class LinkedList {
+//     constructor() {
+//         this.head = null;
+//     }
+
+//     append(value) {
+//         let newNode = new Node(value);
+//         if (!this.head) {
+//             this.head = newNode;
+//             return;
+//         }
+//         let current = this.head;
+//         while (current.next) {
+//             current = current.next;
+//         }
+//         current.next = newNode;
+//     }
+
+//     printList() {
+//         let current = this.head;
+//         let result = "";
+//         while (current) {
+//             result += current.value + "->";
+//             current = current.next;
+//         }
+//         console.log(result + "null");
+//     }
+// }
+
+// let list = new LinkedList();
+// list.append("Paper");
+// list.append("Rock");
+// list.append("Scissors");
+// list.printList();
